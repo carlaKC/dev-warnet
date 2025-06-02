c19355bf7750b24eb2c9b01194e6f0a108f45470
Carla Kirk-Cohen <kirkcohenc@gmail.com>
2025-06-02 14:33:00 -0400

    multi: Add shortcuts and new network for LN setup

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
-## Warnet Dev
+# Warnet Dev
 
 These scripts are primarily for my own use, but can be adapted for 
 setting up test lightning networks using [warnet](https://github.com/bitcoin-dev-project/warnet).
+
+## Once-Off installation
+
+- `source ./new_term.sh`
+- Follow [warnet's installation instructions](https://github.com/bitcoin-dev-project/warnet/blob/main/docs/install.md#install-warnet).
+  - You do not need to create a custom network when you `warnet init`.
+
+## Regular Usage
+
+You *must always* `source ./new_term.sh` to use the shortcuts set up in this repo!
+
+You can do this by:
+- Running `source ./new_term.sh` in every new terminal window.
+- Using [warnet.yml](warnet.yml) tmuxinator template.
+
+## Shortcuts
+
+
+
+❤️ and credit goes to @guggero who created a similar setup for internal use at LL!
+This is entirely inspired by that setup, and I am so grateful for the incredible use I got out of it.
---
+++ new_term.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+python3 -m venv .venv
+source .venv/bin/activate
+
+wd="$(pwd)"
+
+fn start() {
+  echo "Deploying network"
+  warnet deploy $wd
+  echo "Opening Channels"
+  warnet run scenarios/ln_init.py
+  echo "Waiting for gossip to sync"
+  wait_for_scenario "gossip sync"
+}
+
+fn stop() {
+  warnet down
+}
+
+fn logs() {
+  tank_name=$(alias_to_tank "$1") || return 1
+  kubectl logs "$tank_name"
+}
+
+fn cli() {
+  if [ "$#" -ne 2 ]; then
+    echo "Provide node and command: <alice|bob|carol|dave|a|b|c|d> <cli command>" >&2
+    return 1
+  fi
+
+  tank_name=$(alias_to_tank "$1") || return 1
+  warnet ln rpc "$tank_name" "$2"
+}
+
+# Maps user friendly name to tank value.
+fn alias_to_tank() {
+  if [ "$#" -ne 1 ]; then
+    echo "Provide node name: <alice|bob|carol|dave|a|b|c|d>" >&2
+    return 1
+  fi
+
+  name="$1"
+
+  case "$name" in
+    alice|a)  echo "tank-0000-ln" ;;
+    bob|b)    echo "tank-0001-ln" ;;
+    carol|c)  echo "tank-0002-ln" ;;
+    dave|d)   echo "tank-0003-ln" ;;
+    *)
+      echo "Unknown node specify one of: <alice|bob|carol|dave|a|b|c|d>: $name" >&2
+      return 1
+      ;;
+  esac
+}
+
+# Waits for warnet to have no actively running scenarios.
+fn wait_for_scenario() {
+  scenario_name="$1"
+
+  if [ -z "$scenario_name" ]; then
+    echo "Usage: wait_for_scenario <scenario_name>"
+    return 1
+  fi
+
+  echo "Waiting for scenario '$scenario_name' to finish..."
+
+  while true; do
+    output=$(warnet status)
+
+    # Get active scenario count
+    active_scenarios=$(echo "$output" | grep "Active Scenarios" | awk -F': ' '{print $2}')
+
+    if [[ "$active_scenarios" == "0" ]]; then
+      echo "Scenario '$scenario_name' has finished."
+      break
+    else
+      echo "Scenario '$scenario_name' still running"
+      sleep 5
+    fi
+  done
+}
